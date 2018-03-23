95a3acea0c4b8807e66ebc51b5099c4164d8ebd6
Bruno Portes <bruno.portess@hotmail.com>
2018-03-23 09:56:24 -0300

    Add project files.

---
+++ Samples/ViewModels/SearchPageSampleViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Samples.ViewModels
+{
+	public class SearchPageSampleViewModel : BaseViewModel
+	{
+		public SearchPageSampleViewModel()
+		{
+			
+		}
+
+        public ICommand SearchCommand { get; set; }
+        private string _TextSearch;
+        public string TextSearch
+        {
+            get { return _TextSearch; }
+            set {
+                _TextSearch = value;
+                OnPropertyChanged();
+                Debug.WriteLine("SEARCH TEXT: " + value);
+            }
+        }
+        string searchResult;
+
+		public string SearchResult
+		{
+			get
+			{
+				return searchResult;
+			}
+
+			set
+			{
+				searchResult = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
+
